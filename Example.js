cccdbb9e4cfa101a1a166ea3a3be083f76bc47f0
kkprbr <kkpr9963@gmail.com>
2026-09-16 13:50:37 +0530

    Add Example JavaScript program

---
+++ Example.js
@@ -0,0 +1,12 @@
+let a=35;
+let b=45;
+console.log(a);
+console.log(b);
+let x=10;
+for(let i=x;i<20;i++){
+    console.log(i);
+}
+console.log("10");
+console.log("309");
+console.lohl("20");
+console.log("400");
